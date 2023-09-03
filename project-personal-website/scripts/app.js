e7254505ded92c6c5d45b9166427996a1d8b32a1
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-03 22:16:40 +0430

    portfolio section of main finished

--- project-personal-website/scripts/app.js
+++ project-personal-website/scripts/app.js
@@ -2,22 +2,38 @@ const navToggleIcon = document.querySelector(".nav__toggle-icon");
 const menuMobile = document.querySelector(".nav__mobile");
 const cover = document.querySelector(".cover");
 const conditionItems = document.querySelectorAll(".condition__item");
+const portfolioListItems = document.querySelectorAll(".list-portfolio__item");
 navToggleIcon.addEventListener("click", function () {
   this.classList.toggle("nav__toggle-icon--open");
   menuMobile.classList.toggle("nav__mobile--open");
   cover.classList.toggle("cover--show");
 });
-
-conditionItems.forEach((conditionItem) => {
-  conditionItem.addEventListener("click", function () {
-    document
-      .querySelector(".condition__item--active")
-      .classList.remove("condition__item--active");
-    document
-      .querySelector(".resume-sample--show")
-      .classList.remove("resume-sample--show");
-    this.classList.add("condition__item--active");
-    let contentId = this.getAttribute("data-content-id");
-    document.querySelector(contentId).classList.add("resume-sample--show");
+function navigationTabsInit(
+  ListItems,
+  ListItemActiveClass,
+  contentItemShowClass
+) {
+  ListItems.forEach((ListItem) => {
+    ListItem.addEventListener("click", function () {
+      document
+        .querySelector(`.${ListItemActiveClass}`)
+        .classList.remove(ListItemActiveClass);
+      document
+        .querySelector(`.${contentItemShowClass}`)
+        .classList.remove(contentItemShowClass);
+      this.classList.add(ListItemActiveClass);
+      let contentId = this.getAttribute("data-content-id");
+      document.querySelector(contentId).classList.add(contentItemShowClass);
+    });
   });
-});
+}
+navigationTabsInit(
+  conditionItems,
+  "condition__item--active",
+  "resume-sample--show"
+);
+navigationTabsInit(
+  portfolioListItems,
+  "list-portfolio__item--active",
+  "portfolio-content--show"
+);
