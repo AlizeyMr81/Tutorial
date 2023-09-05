1a67bdf1200afa70cbd4ba461cb387f509f61513
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-05 18:13:43 +0430

    Making the active menu while scrolling

--- project-personal-website/scripts/app.js
+++ project-personal-website/scripts/app.js
@@ -3,6 +3,8 @@ const menuMobile = document.querySelector(".nav__mobile");
 const cover = document.querySelector(".cover");
 const conditionItems = document.querySelectorAll(".condition__item");
 const portfolioListItems = document.querySelectorAll(".list-portfolio__item");
+const menuItems = document.querySelectorAll(".menu__item");
+
 navToggleIcon.addEventListener("click", function () {
   this.classList.toggle("nav__toggle-icon--open");
   menuMobile.classList.toggle("nav__mobile--open");
@@ -15,17 +17,16 @@ function navigationTabsInit(
 ) {
   ListItems.forEach((ListItem) => {
     ListItem.addEventListener("click", function () {
-      document
-        .querySelector(`.${ListItemActiveClass}`)
-        .classList.remove(ListItemActiveClass);
-      document
-        .querySelector(`.${contentItemShowClass}`)
-        .classList.remove(contentItemShowClass);
+      removeActiveClass(ListItemActiveClass);
+      removeActiveClass(contentItemShowClass);
       this.classList.add(ListItemActiveClass);
       let contentId = this.getAttribute("data-content-id");
       document.querySelector(contentId).classList.add(contentItemShowClass);
     });
   });
+}
+function removeActiveClass(ClassName) {
+  document.querySelector(`.${ClassName}`).classList.remove(ClassName);
 }
 navigationTabsInit(
   conditionItems,
@@ -37,3 +38,21 @@ navigationTabsInit(
   "list-portfolio__item--active",
   "portfolio-content--show"
 );
+
+
+
+menuItems.forEach(item => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault()
+    removeActiveClass('menu__item--active')
+    item.classList.add("menu__item--active")
+    let sectionClass = item.getAttribute("data-section")
+    let sectionOffsetTop = document.querySelector(`.${sectionClass}`).offsetTop
+
+
+    window.scrollTo({
+      top:sectionOffsetTop -120,
+      behavior : "smooth"
+        })
+  })
+})
