cef5c579c9d0d98c266970c86c297de0fc62e27b
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-11 12:50:59 +0430

    some function to interact whit visitor

---
+++ Javascript/Interaction-alert-prompt-confirm/script.js
@@ -0,0 +1,10 @@
+// Alert function
+alert("Hello,World!");
+
+// Prompt function
+let name = prompt("What is your name", "John");
+alert(`Your name is ${name}`);
+
+// Confirm function
+let isMale = confirm('Is your gender male');
+alert(isMale);
