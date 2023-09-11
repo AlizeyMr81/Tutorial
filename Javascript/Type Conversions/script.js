c1d5f2ea0e6e25c4f00471cb7ea2308b976d5129
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-11 19:58:45 +0430

    Type Conversion

---
+++ Javascript/Type Conversions/script.js
@@ -0,0 +1,19 @@
+// String Conversion
+let bool = true; // boolean
+let stringBool = String(bool);//string "true"
+alert(typeof stringBool)
+
+// Numeric conversion
+let userName = prompt("What is your name:");
+let userAge = Number(userName);
+alert(userAge);
+
+// Boolean Conversion
+let name = "0";
+name = Boolean("0");
+alert(name);
+//---------------
+let menu = 0;
+menu = Boolean(0);
+alert(menu);
+
