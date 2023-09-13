ffd74ec11b57a2244cb9c33aaa2127dba6a992fc
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-13 11:12:52 +0430

    Basic operators in js

---
+++ Javascript/Basic operators/exercise/script.js
@@ -0,0 +1,6 @@
+let firstNumber = prompt("Enter a number", "");
+let secondNumber = prompt("Your score to the site", "");
+let plusResult = `plus result = ${+firstNumber + +secondNumber}`;
+let minusResult = `minus result = ${+firstNumber - +secondNumber}`;
+let zarbResult = `zarb result = ${+firstNumber * +secondNumber}`;
+let tagsimResult = `tagsim result = ${+firstNumber / +secondNumber}`;
---
+++ Javascript/Basic operators/script.js
@@ -0,0 +1,21 @@
+// Remainder %
+let firstNumber = 8;
+let secondeNumber = 3;
+// alert(firstNumber % secondeNumber); // 2
+
+//Exponentiation **
+let b = 2;
+let a = 8;
+// alert(b ** a);
+
+let apples = "5";
+let oranges = "8";
+
+// Increment => ++ / decrement => --
+let counter = 8;
+counter++;
+alert(counter);
+
+// Comma
+let z = (1 + 2, 5 + 8);
+alert(z);
