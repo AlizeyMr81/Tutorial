1b2e760ebb4b1f3c370b2d4ecb7fb3a5ed9896e0
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-13 13:15:37 +0430

    concept of NaN

---
+++ Javascript/Concept of NaN/script.js
@@ -0,0 +1,9 @@
+// Concept of NaN
+// NaN stand for => not a number
+let firstNumber = "Sara ";
+let num1 = 4;
+let num2 = 10;
+alert(firstNumber - num1);
+alert(typeof NaN)
+
+// Typeof NaN is => Number
