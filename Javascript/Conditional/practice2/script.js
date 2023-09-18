c0185401d98730d60aac3cdd6e10d79d0c66cdb3
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-18 19:41:06 +0430

    solve the practice 2

---
+++ Javascript/Conditional/practice2/script.js
@@ -0,0 +1,8 @@
+// سه عدد را از کاربر دریافت بکنید و میانگین آن ها را محاسبه کرده و به کاربر نمایش بدین
+
+let num1 = Number(prompt("Enter first number", ""));
+let num2 = Number(prompt("Enter seconde number", ""));
+let num3 = Number(prompt("Enter third number", ""));
+let average = (num1 + num2 + num3) / 3;
+
+alert(average);
