86ac313bd171f169de04ce36dc5bed76b252a293
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-23 17:32:22 +0330

    Explain about default parameters

--- Javascript/Functions of js/script.js
+++ Javascript/Functions of js/script.js
@@ -40,11 +40,52 @@ function name(parameter1,parameter2,....parameterN) {
 
 // showMessage();
 ///////////////////////////////////////////////////////////// Next part : Parameters
-function showResult(result1, result2) {
-  alert("The first result is : " + result1);
-  alert("The seconde result is : " + result2);
+
+// function showResult(result1, result2) {
+//   alert("The first result is : " + result1);
+//   alert("The seconde result is : " + result2);
+// }
+
+// let name = "Alireza";
+
+// showResult(0 || 5, 7);
+
+////////////////////////////////////////////// Default Parameters :
+
+// function showMessage(userAge, userName = "Alireza") {
+//   alert("My age is : " + userAge);
+//   alert("My name is : " + userName);
+// }
+
+// showMessage(18);
+
+// Alternative default parameters
+
+// function showMessage(userAge, userName) {
+//   alert("My name is : " + userAge);
+//   if (userName === undefined) {
+//     userName = "My name is : " + "Alireza";
+//   }
+//   alert(userName);
+// }
+
+// showMessage(18);
+
+// ----------------------
+
+// function showMessage(userAge, userName) {
+//   alert("My name is : " + userAge);
+//   userName = userName || "My name is : " + "Alireza";
+//   alert(userName);
+// }
+
+// showMessage(18);
+
+// -------------------------
+
+function showCount(count) {
+  alert(count ?? "Empty");
 }
-
-let name = "Alireza";
-
-showResult(0 || 5, 7);
+showCount(null);
+showCount();
+showCount(0);
