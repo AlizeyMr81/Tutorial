33e320a6cb73d8be610af2df95ca2c00f3a42a59
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-26 22:31:37 +0330

    add While loop folder in Loop folder

---
+++ Javascript/Loops/While/script.js
@@ -0,0 +1,35 @@
+// "While" loop :
+
+let i = 0;
+
+while (i < 5) {
+  alert(i);
+  i++;
+}
+
+// if (i < 3) {
+//   alert(i);
+//   i++;
+// }
+// if (i < 3) {
+//   alert(i);
+//   i++;
+// }
+// if (i < 3) {
+//   alert(i);
+//   i++;
+// }
+
+//////////////////////////////////////////
+
+//  "do ... while" loop :
+
+// let num1 = 0;
+// do {
+//   alert(num1);
+//   num1++;
+// } while (num1 < 5);
+
+
+
+
