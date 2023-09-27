1b1a43e45f0fce9729ab6e0d838393b18fdeb8c5
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-09-27 17:57:11 +0330

    Add new code js in For folder in Loop folder

--- Javascript/Loops/For/script.js
+++ Javascript/Loops/For/script.js
@@ -10,10 +10,48 @@ for (begin; condition ; step) {
  
  */
 
-let i = 0;
+// let i = 0;
 
-for (i = 0; i < 3; i++) {
-  alert(i);
+// for (i = 0; i < 3; i++) {
+//   alert(i);
+// }
+
+// alert(i); // show number 3
+
+// ----------------- Break :
+
+// let sum = 0;
+
+// for (; true; ) {
+//   let value = +prompt("Enter your number", "");
+
+//   if (!value) {
+//     break;
+//   } else {
+//     sum += value;
+//   }
+// }
+
+// alert("Sum : " + sum);
+
+// ------------------------ Continue :
+
+// for (let value = 0; value < 12; value++) {
+//   if (value % 2 === 0) {
+//     continue;
+//   }
+
+//   alert(value); // 1, then 3, 5, 7
+// }
+
+// ------------------------------------label for break / continue :
+
+outer: for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    let input = prompt(`Value at coords (${i} , ${j})`, "");
+    if (!input) {
+      break outer;
+    }
+  }
 }
-
-alert(i); // show number 3
+alert("\"Done\"");
