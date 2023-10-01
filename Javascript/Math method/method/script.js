12990ec52b806c3e19a833a6ec1830c3d6ca0087
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-10-01 12:47:21 +0330

    Math methods folder

---
+++ Javascript/Math method/method/script.js
@@ -0,0 +1,33 @@
+// Math :
+
+console.log(Math.PI);
+
+console.log(Math.pow(5, 2));
+
+console.log(Math.sqrt(25));
+
+console.log(Math.abs(-15));
+
+console.log(Math.min(12, 156, 1253, 35));
+
+console.log(Math.max(12, 156, 1253, 35));
+
+console.log(Math.floor(-1.6));
+
+console.log(Math.trunc(-1.6));
+
+console.log(Math.ceil(2.2));
+
+console.log(Math.round(6.5));
+
+console.log(Math.random());
+
+console.log(Math.acos(0)); //Returns the arccosine of x, in radians
+
+console.log(Math.asin(0)); //Returns the arcsine of x, in radians
+
+console.log(Math.sign(1551)); //Returns if x is negative, null or positive (-1, 0, 1)
+
+console.log(Math.cbrt(216)); //returns the cubic root of a number.
+
+// and etc....
