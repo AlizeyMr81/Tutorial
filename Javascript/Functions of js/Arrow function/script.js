7d46e691cb67096dc1f55d128e23fa3d367c92ab
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-10-07 09:42:39 +0330

    explain about arrow function

---
+++ Javascript/Functions of js/Arrow function/script.js
@@ -0,0 +1,23 @@
+//  Arrow Function :
+
+/*
+syntax : 
+
+let x = (arg1 , agr2 ,.... , argN) => expression;
+
+let x = (arg1 , agr2 ,.... , argN) => { body }
+
+*/
+
+// let sum = (a, b) => a + b;
+
+// console.log(sum(2, 3));
+
+// Multiline Arrow function :
+
+let sum = (a, b) => {
+  let result = a + b;
+  return result;
+};
+
+console.log(sum(2, 8));
