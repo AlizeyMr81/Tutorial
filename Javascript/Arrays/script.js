95d320439380903ae9a44fa641f8ecc1bab62c1f
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-10-08 11:33:51 +0330

    basic structure of arrays

---
+++ Javascript/Arrays/script.js
@@ -0,0 +1,18 @@
+// Arrays :
+
+/* 
+    let arr = new Array();
+
+    let arr = []
+
+*/
+
+let car = ["Benz", "BMW", "Audi"];
+
+// car[1] = "Rolls Royce";
+
+// car[3] = "BMW";
+
+// car[4] = 52;
+
+console.log(car[2]);
