eb2a7d1f3cba5b3578ec26f8055ffe2822707d65
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-10-14 20:44:40 +0330

    add new code in script file of object

--- Javascript/Objects/script.js
+++ Javascript/Objects/script.js
@@ -39,18 +39,95 @@ let user = {};
 
 ////////////////////////////////////////////////////////////////////////// delete operator :
 
+// let user = {
+//   name: "Ali",
+//   age: 21,
+//   "last name": "Zeighampour",
+// };
+
+// console.log(user);
+
+// delete user["last name"];
+
+// console.log(user);
+
+// user.isAdmin = true;
+
+// console.log(user);
+
+///////////////////////////////////////////////////////
+
+// let user = {
+//   name: "John",
+//   age: 21,
+// };
+
+// let key = prompt("what do you want about user","name");
+
+// alert(user[key])
+
+/////////////////////////////////////////////////////////// Computed properties :
+
+// let fruit = prompt("Enter your a name of fruit", "apple");
+
+// let bag = {};
+// bag[fruit] = 5;
+
+// console.log(bag);
+
+// ------- Or
+
+// let user = prompt("Enter name John");
+
+// let userName = {
+//   [user]: 5,
+// };
+
+// console.log(userName.John);
+
+// ------------------------------------- Property value shorthand :
+
+// function makeUser(name, age) {
+//   return {
+//     name,
+//     age,
+//   };
+// }
+
+// let user = makeUser("John", 31);
+
+// console.log(user);
+
+//---------------------------- Property names limitations :
+
+// let obj = {
+//   for: 1,
+//   return: 2,
+//   let: 6,
+// };
+
+// console.log(obj["for"]);
+
+// ------------------------------------- Property existence test, “in” operator :
+// syntax : "key" in object
+
+// let user = {
+//   name: "ali",
+//   age: 25,
+// };
+
+// console.log("name" in user);
+// console.log("family" in user);
+
+// ------------------------------------------------ for...in :
+
 let user = {
-  name: "Ali",
-  age: 21,
-  "last name": "Zeighampour",
+  name: "ali",
+  age: 32,
+  family: "Bahrami",
 };
 
-console.log(user);
-
-delete user["last name"];
-
-console.log(user);
-
-user.isAdmin = true;
-
-console.log(user);
+for(let key in user) {
+  alert(key);
+  alert(user[key])
+}
