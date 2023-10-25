050d0e91e5e7a47eef506f98cb083e432adbc16d
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-10-25 13:09:04 +0330

    add new code in forEach method script file

--- Javascript/Arrays/forEach-method/script.js
+++ Javascript/Arrays/forEach-method/script.js
@@ -5,8 +5,20 @@ users.forEach(function () {
   console.log("Alireza");
 });
 
-
 let color = ["Red", "Blue", "Purple", "Brown", "Green"];
 color.forEach(function (colorName) {
-    console.log(colorName);
+  console.log(colorName);
+});
+
+// Index (optional) :
+
+let numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function (value, index) {
+  console.log("Value : " + value + "\n" + "Index : " + index);
+});
+
+// Array (optional) :
+
+numbers.forEach(function (value, index, array) {
+  console.log(array);
 });
