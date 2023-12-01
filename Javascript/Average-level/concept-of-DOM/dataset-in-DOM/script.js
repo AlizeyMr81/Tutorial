fbf42028bd480b65eb172f4c615dea821542f71b
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-12-01 23:10:02 +0330

    Explanation about dataset property

---
+++ Javascript/Average-level/concept-of-DOM/dataset-in-DOM/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(event) {
+//    console.log(event.target.dataset.name);
+// })
+
+console.log(btn.dataset);
+console.log(btn.dataset.name);
+console.log(btn.dataset.color);
