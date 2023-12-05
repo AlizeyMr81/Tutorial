1e7548e4d3639020159403a66b22e4d1746d3c0c
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2023-12-05 18:14:19 +0330

    completed explain about drag and drop

--- Javascript/Average-level/concept-of-DOM/Drag-Drop/script.js
+++ Javascript/Average-level/concept-of-DOM/Drag-Drop/script.js
@@ -3,11 +3,23 @@ let $ = document;
 const h1Elements = $.querySelectorAll("h1");
 const divDrop = $.querySelector(".drop");
 
+// Dragged -> onDragstart - ondrag - onDragEnd
+// dropped -> onDargEnter - onDargOver - onDragLeave - onDrop
+
 h1Elements.forEach(function (items) {
   items.draggable = true;
 
   items.addEventListener("dragstart", function (event) {
     event.dataTransfer.setData("elemId", event.target.id);
+    // console.log("Drag Start");
+  });
+
+  items.addEventListener("drag", function () {
+    // console.log("Drag");
+  });
+
+  items.addEventListener("dragend", function () {
+    // console.log("Drag End");
   });
 });
 
@@ -16,8 +28,11 @@ divDrop.addEventListener("drop", function (event) {
   let targetElement = document.getElementById(elementID);
 
   divDrop.append(targetElement);
+
+  console.log("Drop");
 });
 
 divDrop.addEventListener("dragover", function (event) {
   event.preventDefault();
+  console.log("Drag Over");
 });
