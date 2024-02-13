da4b033b3431cde7017983b857f26ca55e4d1edf
Alireza Zeyghampour <zeyghampouralireza@gmail.com>
2024-02-13 09:48:38 +0330

    explain data object

---
+++ Javascript/Average-level/concept-of-DOM/Date-Object/script.js
@@ -0,0 +1,10 @@
+let myTime = new Date();
+
+console.log(myTime);
+console.log(myTime.getFullYear());
+console.log(myTime.getMonth());
+console.log(myTime.getDate());
+console.log(myTime.getHours());
+console.log(myTime.getMinutes());
+console.log(myTime.getSeconds());
+let divelem = document.query
